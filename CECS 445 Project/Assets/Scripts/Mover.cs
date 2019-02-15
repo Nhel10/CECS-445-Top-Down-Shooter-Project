fda9b7053eec37f4970082007e25f83dd1bf8b1f
Hayden Matheus <haydenh2o@yahoo.com>
2019-02-14 16:57:00 -0800

    Add files via upload
    Very basic player weapon. Pretty much a bullet hose. I can refine it more once we get a more unified prototype to work off of.

---
+++ CECS 445 Project/Assets/Scripts/Mover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rigidBody;
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = transform.up * speed;
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ CECS 445 Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject projectile;
+
+    public Transform projectileSpawn;
+
+    private float nextShot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKey && Time.time > nextShot)
+        {
+            Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+        }
+	}
+}
