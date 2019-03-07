d6b44dd38d323a1c9754e5b3b063e9de8a1c8b00
chanito2 <fett22@rocketmail.com>
2019-03-07 10:13:55 -0800

    Yes

---
+++ New Unity Project (2)/Assets/Float.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour
+{
+    public float speed = 3f;
+    public RigidBody2D asteroid;
+
+    //will be executed once at script start
+    void Start()
+    {
+        asteroid = GetComponent<Rigidbody2D>();
+        asteroid.velocity = speed * (new Vector3(0, -1, 0));
+    }
+
+    //will be executed if gameobject is not rendered anymore on screen 
+    void Destroy()
+    {
+        //delete gameobject from scene
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-Input.GetAxis("Vertical") * speed * Time.deltaTime, 0, 0);
+    }
+
+}
---
+++ New Unity Project (2)/Assets/Scenes/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnWaves();
+    }
+
+    // Update is called once per frame
+    void SpawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
