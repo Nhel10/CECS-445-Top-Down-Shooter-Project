4f8892ee24cb1fd17d42d1d34a9755174e6ed9cd
Neru <lnyonzon@gmail.com>
2019-03-13 20:10:13 -0700

    Added shredder objects
    Shredder object will delete enemies whern they reach offscreen so they dont linger and take up space and memory

---
+++ CECS 445 Project/Assets/Scripts/Shredder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour{
+
+  void onTriggerEnter2D(Collider2D col){
+    Destroy(col.gameObject);
+  }
+
+}
