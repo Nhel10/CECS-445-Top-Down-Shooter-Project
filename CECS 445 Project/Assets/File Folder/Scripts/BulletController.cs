7f2c53b8457a1b16028fb57ee64c8b5aac030bfe
Neru <lnyonzon@gmail.com>
2019-03-13 21:04:52 -0700

    Moved all files into one folder

---
+++ CECS 445 Project/Assets/File Folder/Scripts/BulletController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D bullet;
+
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // just move forward in the direction of the ship
+        transform.Translate(new Vector2(0,1) * speed * Time.deltaTime);
+    }
+
+    // destroy bullet when reaching outer screen
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    // destroy bullet when colliding any 2D collider or rigidbogy
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // check if the 2D colliding object is an Enemy
+        if (collision != null && 
+        (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Asteroid"))
+        {
+            // remove 'damage' amount of life to the enemy
+            collision.gameObject.GetComponent<HealthManager>().GetHurt(damage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ CECS 445 Project/Assets/File Folder/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+    public SimpleHealthBar healthBar;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.UpdateBar( currentHealth, maxHealth );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void GetHurt(float damage)
+    {
+        currentHealth -= damage;
+        healthBar.UpdateBar( currentHealth, maxHealth );
+    }
+}
