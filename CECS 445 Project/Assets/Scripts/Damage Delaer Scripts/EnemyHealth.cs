62445c817cf82174a88d57996ccf46b8aee0985d
Neru <lnyonzon@gmail.com>
2019-04-17 03:40:39 -0700

    Added new enemy type
    Added a new enemy type to implement  Dongs features in Nicolas's enemy sprites

---
+++ CECS 445 Project/Assets/Scripts/Damage Delaer Scripts/EnemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    [SerializeField] float health = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEvent2D(Collider2D other){
+      DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+      health -= damageDealer.GetDamage();
+    }
+}
